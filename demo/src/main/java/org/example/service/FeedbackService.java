817b2a541b688c3915eec969fe4807e74b516dac
pkh129110 <2397299675@qq.com>
2026-06-03 13:32:32 +0800

    我收到的及卖出的-持久层、服务层修改

--- demo/src/main/java/org/example/service/FeedbackService.java
+++
@@ -1,7 +0,0 @@
-package org.example.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import org.example.entity.Feedback;
-
-public interface FeedbackService extends IService<Feedback> {
-}
